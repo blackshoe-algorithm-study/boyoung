83812b91d0262070ccbaba4da1fe26e1bad30057
BoyoungH <hyong0903@naver.com>
2024-03-24 16:24:05 +0900

    Solve: 수_정렬하기2

--- week1_0320/문자열_반복.java
+++ week1_0320/문자열_반복.java
@@ -8,19 +8,20 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<T; i++){
             String[] str = br.readLine().split(" ");
-            int num = Integer.parseInt(str[0]); // 2
-            String alpha = str[1]; // abc
-            for(int j=0; j<alpha.length(); j++){ // 문자 개수
-                for(int k=0; k<num; k++){ 
-                    sb.append(alpha.charAt(j));
+            int R = Integer.parseInt(str[0]);
+            String S = str[1];
+            for(int j=0; j<S.length(); j++){
+                for(int k=0; k<R; k++){ 
+                    sb.append(S.charAt(j));
                 }
             }
             sb.append("\n");
         }
+        br.close();
         System.out.print(sb);
     }
 }
---
+++ week1_0320/수_정렬하기2.java
@@ -0,0 +1,31 @@
+package week1_0320;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main2 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int num : list){
+            sb.append(num);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
