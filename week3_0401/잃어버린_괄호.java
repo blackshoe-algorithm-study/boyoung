4d93564b1149b2cd10cb4c3bf8eda89ea9f9ee39
BoyoungH <hyong0903@naver.com>
2024-04-01 13:41:58 +0900

    Fix: week3_0401

--- week3_0401/잃어버린_괄호.java
+++ week3_0401/잃어버린_괄호.java
@@ -8,16 +8,22 @@ import java.util.StringTokenizer;
 public class 잃어버린_괄호 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine(),"-");
-        int first=0;
-        int sum = 0;
-        while(st1.hasMoreTokens()){ // -로 연결된 연산까지 수행
-            StringTokenizer st2 = new StringTokenizer(br.readLine(),"+");
-            while(st2.hasMoreTokens()){ // +로 연결된 연산
-                sum += Integer.parseInt(st2.nextToken());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), "-");
+        int result = Integer.MAX_VALUE;
+
+        while(st1.hasMoreTokens()){
+            int temp = 0;
+            StringTokenizer st2 = new StringTokenizer(st1.nextToken(), "+");
+            while(st2.hasMoreTokens()){
+                temp+=Integer.parseInt(st2.nextToken());
+            }
+            if(result == Integer.MAX_VALUE){
+                result = temp;
+            }
+            else{
+                result -= temp;
             }
         }
-        
-
+        System.out.print(result);        
     }
 }
--- week3_0401/회의실_배정.java
+++ week3_0401/회의실_배정.java
@@ -20,19 +20,18 @@ public class 회의실_배정 {
         }
         //오름차순  -> 검색
         Arrays.sort(time, (meet1, meet2) -> {
-            if(meet1[1] == meet2[1]){
-                return meet1[0] - meet2[0];
+            if(meet1[1] == meet2[1]){ // 종료시간이 같을 경우
+                return meet1[0] - meet2[0]; // 시작 시간이 빠른 순으로 정렬
             }
-            return meet1[1] - meet2[1]; //내림차순 : meet2[1] - meet1[1]
+            return meet1[1] - meet2[1]; //종료시간 기준 내림차순 : meet2[1] - meet1[1]
         });
         //반복문 
         int count=1;
-        for(int i=0; i<N; i++){
-            for(int j=i; j<N; j++){
-                if(time[i][1] < time[j][0]){
-                    i=j;
-                    count+=1;
-                }
+        int end = time[0][1];
+        for(int i=1; i<N; i++){
+            if(time[i][0] >= end){
+                end = time[i][1];
+                count++;
             }
         }
         System.out.print(count);
