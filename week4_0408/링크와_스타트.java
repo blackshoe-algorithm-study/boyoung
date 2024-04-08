51c31870810520657f8552e16e9225d74f79829f
BoyoungH <hyong0903@naver.com>
2024-04-08 17:56:40 +0900

    Fix: week4_0408

--- week4_0408/링크와_스타트.java
+++ week4_0408/링크와_스타트.java
@@ -6,39 +6,58 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 링크와_스타트 {
+    static int N, result;
+    static int[][] S;
+    static boolean[] check;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][N];
-        boolean[] check = new boolean[N]; // 같은 팀에 속해있는지 판단하기 위함
+        N = Integer.parseInt(br.readLine());
+        S = new int[N][N];
+        check = new boolean[N]; // 같은 팀에 속해있는지 판단하기 위함
+        result = Integer.MAX_VALUE;
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                S[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        // N개일떄, (1 개, N-1 개 ), (2개, N-2개), (3개, N-3개) ...
-        for(int i=1; i<N; i++){
-            solution(i, check, arr);
-        }     
+        solution(0);
+        System.out.println(result); 
     }
-
-    public static void solution(int idx, boolean[] check, int[][] arr){
-        int start = 0;
-        int end = 0;
-
-        for(int i=0; i<idx; i++){
-            for(int j=i+1; j<idx; j++){
-                if(check[i] == true && check[j] == true) { 
-                    start += arr[i][j] + arr[j][i];
-                }
-                else if(check[i] == false && check[j] == false) { 
-                    end += arr[i][j] +arr[j][i];
+    
+    //부분집합 계산
+    public static void solution(int count){
+        if(count == N){ // 모든 선수들의 소속이 정해진 경우 능력치 계산 로직
+            int start = 0;
+            int link = 0;
+            
+            //모든 선수들의 소속을 확인하여 쌍으로 능력치 계산
+            for(int i=0; i<N; i++){
+                for(int j=i+1; j<N; j++){
+                    if(check[i] != check[j]) continue;
+                    if(check[i]) { 
+                        start += S[i][j] + S[j][i];
+                    }
+                    else{
+                        link += S[i][j] +S[j][i];
+                    }
                 }
             }
+
+            int diff = Math.abs(start - link);
+            if(diff < result) result = diff;
+
+            return;
         }
+        //재귀함수 사용
+
+        check[count] = true; //현재 인덱스의 선수를 팀에 포함할때
+        solution(count + 1);
+        check[count] = false;//현재 인덱스의 선수를 팀에 포함하지 않을때
+        solution(count + 1);
+        // 결국에는 전부다 한팀에 몰빵되는 결과(전부 true or false)도 계산이 되지만
+        // 위의 계산에서 어차피 최소값을 고르는 경우에서 걸러짐
     }
     
 }
--- week4_0408/퇴사.java
+++ week4_0408/퇴사.java
@@ -1,9 +1,41 @@
 package week4_0408;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 퇴사 {
+    static int N;
+    static int[][] arr;
+    static int result = 0;
     public static void main(String[] args) throws IOException{
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        solution(0, 0);
+
+        System.out.println(result);
     }   
+
+    public static void solution(int i, int profit){
+        if(i >= N){
+            if(result < profit){
+                result = profit;
+            }
+            return;
+        }
+        if(i + arr[i][0] <= N){
+            solution(i + arr[i][0], profit + arr[i][1]);
+        }
+        else{
+            solution(i + arr[i][0], profit);
+        }
+        solution(i + 1, profit);
+    }
 }
