4e3af376732376ddfc68a797ae920097b6b3e0ee
BoyoungH <hyong0903@naver.com>
2024-04-17 17:24:49 +0900

    Add : week5_0415

--- week5_0415/소수의_연속합.java
+++ week5_0415/소수의_연속합.java
@@ -20,16 +20,43 @@ public class 소수의_연속합 {
 
         for(int i=2; i*i<N+1; i++){
             if(isPrime[i]){
-                for(int j=i*i; j<N+1; j+=i){
+                for(int j=i*2; j<N+1; j+=i){
                     isPrime[j] = false;
                 }
             }
         }
 
-        for(int i=1; i<N+1; i++){
+        for(int i=2; i<N+1; i++){
             if(isPrime[i]){
                 list.add(i);
             }
         }
+
+        // 투 포인터
+
+        int left = 0;
+        int right = 0;
+        int sum = 2;
+        int count = 0;
+        //N의 값보다 합이 작으면 right를 올려서 더해주고
+        //크면 left를 올려서 빼주고
+        //left와 right가 같아지면 끝
+
+        while(left < list.size() && right < list.size()){
+            if(sum < N){
+                if(right + 1 >= list.size()) break;
+                sum += list.get(++right);
+            }
+            else if(sum > N){
+                sum -= list.get(left++);
+            }
+            else{
+                count++;
+                sum -= list.get(left++);
+            }
+        }
+
+        System.out.println(count);
+
     }
 }
